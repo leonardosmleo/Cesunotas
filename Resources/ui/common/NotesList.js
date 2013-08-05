615677aab026089a563679eea9dc9306012bbe15
Jair <web@layoutz.com.br>
2013-08-04 23:13:59 -0300

    Primeiro commit.

---
+++ Resources/ui/common/NotesList.js
@@ -0,0 +1,524 @@
+/**
+ * @author jmilanes
+ */
+
+var diciplinsWindows;
+var currentWindow = -1;
+var user = false;
+function NotesList(notes)
+{
+	//Ti.API.debug(JSON.stringify(notes));
+	
+	notes = JSON.parse(notes);
+	user = notes['user'];
+	delete notes['user'];
+
+	//var tabGroup = Ti.UI.createTabGroup();
+
+	var notesListWindow = Ti.UI.createWindow({
+		title:'Cesunotas',
+		backgroundColor: '#161616',
+		tabBarHidden:true,
+		navBarHidden:true,
+		exitOnClose: true
+	});
+	
+	var disciplinsLabelContainer = getHeaderLayout(user);
+	
+	notesListWindow.add(disciplinsLabelContainer);
+
+	var listView = prepareListView( notes['notes']);
+
+	notesListWindow.add( listView );
+	
+	notesListWindow.add( getFooterTemplate() );
+	
+	notesListWindow.add(activityIndicator);
+	
+	notesListWindow.open();
+
+	return notesListWindow;
+}
+
+function getHeaderLayout(user, title){
+	if( !title ){
+		title = 'Disciplinas';
+	}
+	var disciplinsLabelContainer = Ti.UI.createView({
+		top: 0,
+	    backgroundColor: '#EEEEEE',
+	    width: '100%', height: 90
+	});
+
+	var disciplinsLabel = Ti.UI.createLabel({
+	    color: '#404E57',
+	    font: { fontSize:'13dp',fontFamily:'Helvetica Neue' },
+	    shadowColor: '#000',
+	    shadowOffset: {x:5, y:5},
+	    text: user,
+	    textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+	    top: 10,
+	    left: '10dp'
+	});
+	
+	var headerLogo = Ti.UI.createImageView({
+		image: '/images/logo.png',
+		height: '15dp',
+		right: '10dp',
+		top: 15
+	});
+	
+	var disciplinsSectionLabelContainer = Ti.UI.createView({
+		top: 60,
+	    backgroundColor: '#404E57',
+	    width: '100%', height: 30
+	});
+	
+	var disciplinsSectionLabel = Ti.UI.createLabel({
+	    color: '#fff',
+	    font: { fontSize:'13dp',fontFamily:'Helvetica Neue' },
+	    backgroundColor: '#404E57',
+	    shadowColor: '#000',
+	    shadowOffset: {x:5, y:5},
+	    text: title,
+	    textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+	    top: 0,
+	    left: '10dp',
+	    bottom: 0
+	});
+	
+	disciplinsLabelContainer.add(disciplinsLabel);
+	disciplinsLabelContainer.add(headerLogo);
+	
+	disciplinsSectionLabelContainer.add(disciplinsSectionLabel)
+	disciplinsLabelContainer.add(disciplinsSectionLabelContainer);
+	
+	
+	return disciplinsLabelContainer;
+}
+
+function prepareListView( notes )
+{
+	//Ti.API.debug( JSON.stringify( notes ) );
+	var periods = notes['Disciplina'];
+	delete notes['Disciplina'];
+
+	var disciplins = [];
+	var nots = [];
+	var difs = [];
+	for( var disciplin in notes ){
+		
+		var name = disciplin.split('-');
+		code = name[1].replace(/^s+|s+$/g,"");
+		name = name[0];
+		disciplins.push( { 
+			pic:{
+				image: '/icons/dark_2x/dark_arrow-closed@2x.png', opacity: 0.3
+			},
+			code:{
+				text: code
+			},
+			info:{ 
+				text: abreviateName(name), 
+			} 
+		} );
+
+		var nos = extractNotes(notes[disciplin]);
+		nots.push(nos.notes);
+		difs.push(nos.difs);
+	}
+
+	var section = getNotesListSection(disciplins);
+
+	var notesListView = getNotesListView(section);
+
+	notesListView.setSections( [section] );
+
+	diciplinsWindows = preparePeriodsListViews(section,periods,nots,difs);
+
+	notesListView.addEventListener('itemclick', function(e){
+    	var item = section.getItemAt(e.itemIndex);
+    	//Ti.API.debug(var_dump(e));
+    	if( typeof item.properties !== 'undefined' ){
+			item.properties.opacity = 1;
+		} else {
+			item.properties = { opacity: 1};
+		}
+		
+    	section.updateItemAt(e.itemIndex, item );
+    	
+    	setTimeout(function(){
+			item.properties.opacity = 0.9;
+			section.updateItemAt(e.itemIndex, item );
+    	},1000);
+    	
+    	openPeriodViewByDiciplinIndex(e.itemIndex);
+    	
+    	//Ti.API.debug(var_dump(item));
+    	//item.properties.backgroundColor = '#000';
+  	});
+
+	return notesListView;
+	
+}
+
+function getNotesListView(){
+	return Ti.UI.createListView({
+	    // Maps myTemplate dictionary to 'template' string
+	    templates: { 'template': getDiciplinsItemsTemplate() },
+	    // Ue 'template', that is, the myTemplate dict created earlier
+	    // for all items as long as the template property is not defined for an item.
+	    defaultItemTemplate: 'template',
+	    top: 90,
+	    bottom: 50,
+	    backgroundColor: '#000'
+	});
+}
+
+function getPeriodsListView(){
+	return Ti.UI.createListView({
+	    // Maps myTemplate dictionary to 'template' string
+	    templates: { 'template': getPeriodsItemsTemplate() },
+	    // Ue 'template', that is, the myTemplate dict created earlier
+	    // for all items as long as the template property is not defined for an item.
+	    defaultItemTemplate: 'template',
+	    top: 90,
+	    bottom: 100,
+	    backgroundColor: '#fff',
+	    borderColor: '#efefef'
+	});
+}
+
+function getNotesListSection(diciplinsItems){
+	var section = Ti.UI.createListSection();
+	section.setItems( diciplinsItems );
+	return section;
+}
+
+function getPeriodsListSection(periods){
+	var section = Ti.UI.createListSection();
+	section.setItems( periods );
+	return section;
+}
+
+function extractNotes(notes){
+	var ns = [];
+	var difs = [];
+	for( var i=0; i < notes.length; i++ ){		
+		if( typeof notes[i] == 'string' ){
+			var note = notes[i].replace(',','.');
+			note = parseFloat( note );
+		} else {
+			note = notes[i];
+		}
+		note = new Number( note );
+		note = note.toFixed(1);
+		
+		if( i > 0 ){
+			if( note > 0 ){
+				var previus = parseFloat( notes[( i - 1 )].replace(',','.') );
+				difs[i] = new Number( Math.abs( note - previus ) );
+				difs[i] = difs[i].toFixed(1);
+				if( note > previus ){
+					difs[i] = '+'+difs[i]+'';
+				} else {
+					difs[i] = '-'+difs[i]+'';
+				}
+			}
+		}
+		ns.push(note);
+	}
+	return { notes: ns, difs: difs };
+}
+
+function prepareDiciplinsListItemView(){
+	
+}
+
+function preparePeriodsListViews(section,periods,nots,difs){
+	
+	var results = [];
+	
+	for( var i=0; i<nots.length; i++){
+		
+		//var list = new getPeriodsListView();	
+		var rows = [];
+		for( var b=0; b < nots[i].length; b++){
+			var dif = '';
+			if( b > 0 && nots[i][b] > 0 ){
+				dif = difs[i][b];
+			}
+			rows.push({ 
+				period:{text: periods[b]},
+				note:{ 
+					text: nots[i][b],
+					color: ( ( nots[i][b] < 6.5 ) ? ((nots[i][b] < 3.5 )? ( (nots[i][b] == 0)? '#ccc' : '#9F1313' ) : '#D96D00' ) : '#447F17' )
+				},
+				dif:{ 
+					text: dif,
+					color: ( dif.indexOf("+") ) ? '#9F1313' : '#447F17'
+				}
+			});
+		}
+		
+		
+		//list.setSections([section]);
+
+		results.push(rows);
+	}
+	return results;
+}
+var click_active = false;
+function openPeriodViewByDiciplinIndex(index)
+{
+	if( !click_active ){
+		click_active = true;
+
+		//Ti.API.debug(index);
+		
+		var currentWin = Ti.UI.currentWindow;
+		
+		//Ti.API.debug(currentWin.visible);
+		//Ti.API.debug(currentWin);
+		//Ti.API.debug(diciplinsWindows);
+		
+		var list = new getPeriodsListView();	
+		var section = getPeriodsListSection(diciplinsWindows[index]);
+		list.setSections([section]);
+		//list.setSections([diciplinsWindows[index]]);
+
+		//Ti.API.debug(list);
+
+		if( typeof list !== 'object' ){
+			return false;
+		}
+		
+		var sec = list.getSections();
+		
+		var total = 0;
+		var avgRatio = 0;
+		var avg = 0;
+		if( typeof sec !== 'undefined' ){
+			
+			var items = sec[0].getItems();
+			//Ti.API.debug();
+			for( var i=0; i<items.length; i++ ){
+				var note = parseFloat(items[i].note.text);
+				if( note > 0 ){
+					total = total + note;
+					avgRatio++;
+				}
+			}
+			total = new Number( total );
+			total = total.toFixed(1);
+			if( total > 0 ){
+				avg = total / avgRatio;
+			} else {
+				avg = '';
+			}
+		}
+		//Ti.API.debug( JSON.stringify(items) );
+
+		var header = new getHeaderLayout( user );
+		var footer = new getPeriodsFooter( total, avg );
+		//return false;
+	
+		//Ti.API.debug(JSON.stringify(list));
+		
+		var win = new getNewWindowForPeriods(index,list);
+		win.add(header);
+		win.add(list);
+		win.add(footer);
+		
+		//Ti.API.debug(list);
+		
+
+		click_active = false;
+		win.open({animated:true});
+		
+	}
+	//return false;
+	return win;
+}
+
+function getNewWindowForPeriods(index,list){
+	var win = Ti.UI.createWindow({
+				title:'Cesunotas_'+index,
+				backgroundColor: '#161616',
+				tabBarHidden:true,
+				navBarHidden:true,
+				exitOnClose: false,
+				modal: true,
+				bubbleParent: false
+				//left: Ti.Platform.displayCaps.platformWidth
+		});
+	win.addEventListener('androidback',function(e){
+			e.cancelBubble = true;
+			win.close();
+			win.remove(list);
+			win = null;
+		});
+	return win;
+}
+
+function getPeriodsFooter( total, avg ){
+	var footerContainer = Ti.UI.createView({
+		bottom: 0,
+	    backgroundColor: '#404E57',
+	    width: '100%', height: 100
+	});
+	
+	var footerTotalLabel = Ti.UI.createLabel({
+	    color: '#999',
+	    font: { fontSize:'20dp',fontFamily:'Helvetica Neue' },
+	    shadowColor: '#000',
+	    shadowOffset: {x:5, y:5},
+	    text: 'Total',
+	    textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+	    top: 10,
+	    left: '10dp'
+	});
+
+	var footerTotalValue = Ti.UI.createLabel({
+	    color: '#fff', // ( ( total < 26 ) ? ((total < 13 )? '#FF4D4D' : '#D96D00' ) : '#447F17' ),
+	    font: { fontSize:'45dp',fontFamily:'Helvetica Neue' },
+	    shadowColor: '#000',
+	    shadowOffset: {x:5, y:5},
+	    text: total,
+	    textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+	    top: 10,
+	    left: '100dp',
+	    bottom: 10
+	});
+	
+	var footerTotalAvg = Ti.UI.createLabel({
+	    color: ( ( total < 5 ) ? '#FF4D4D' : '#447F17' ),
+	    font: { fontSize:'30dp',fontFamily:'Helvetica Neue' },
+	    shadowColor: '#000',
+	    shadowOffset: {x:5, y:5},
+	    text: avg,
+	    textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+	    left: '220dp',
+	    top: 15
+	});
+	footerContainer.add(footerTotalLabel);
+	footerContainer.add(footerTotalValue);
+	footerContainer.add(footerTotalAvg);
+	return footerContainer;
+}
+
+function getPeriodsItemsTemplate(){
+	return  {
+	    childTemplates: [
+	       {                            // Title 
+	            type: 'Ti.UI.Label',     // Use a label for the title 
+	            bindId: 'period',          // Maps to a custom info property of the item data
+	            properties: {            // Sets the label properties
+	                color: '#666',
+	                font: { fontFamily:'Helvetica Neue', fontSize: '20dp' },
+	                left: '15dp', top:15
+	            }
+	       }, 
+	        {                            // Subtitle
+	            type: 'Ti.UI.Label',
+	            bindId: 'note',  
+	            properties: {            // Sets the label properties
+	                font: { fontFamily:'Helvetica Neue', fontSize: '45dp' },
+	                left: '100dp', 
+	                top:10,
+	                bottom: 10,
+	                color: '#025F8B'
+	            }
+	        },
+	        {                            // Title 
+	            type: 'Ti.UI.Label',     // Use a label for the title 
+	            bindId: 'dif',          // Maps to a custom info property of the item data
+	            properties: {            // Sets the label properties
+	                color: '#666',
+	                font: { fontFamily:'Helvetica Neue', fontSize: '30dp' },
+	                left: '180dp', top: 15
+	            }
+	       },
+	    ]
+ 	};
+}
+
+function getDiciplinsItemsTemplate(){
+	return  {
+		properties: {
+			backgroundColor: '#fff',
+			opacity: 0.9
+			//accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_DETAIL
+		}, 
+		events: {
+			touchend: function(e){    
+			},
+			touchstart: function(e){	
+			}
+		},
+	    childTemplates: [
+	        {                            // Image justified left
+	            type: 'Ti.UI.ImageView', // Use an image view for the image
+	            bindId: 'pic',           // Maps to a custom pic property of the item data
+	            properties: {            // Sets the image view  properties
+	                width: '30dp', height: '30dp', right: '15dp', top: 25
+	            }
+	        },
+	        {                            // Title 
+	            type: 'Ti.UI.Label',     // Use a label for the title 
+	            bindId: 'code',          // Maps to a custom info property of the item data
+	            properties: {            // Sets the label properties
+	                color: '#666',
+	                font: { fontFamily:'Helvetica Neue', fontSize: '13dp' },
+	                left: '10dp', top: 15, width: width - 80
+	            }
+	        }, 
+	        {                            // Subtitle
+	            type: 'Ti.UI.Label',
+	            bindId: 'info',  
+	            properties: {            // Sets the label properties
+	                font: { fontFamily:'Helvetica Neue', fontSize: '17dp' },
+	                left: '10dp', 
+	                top: '35dp',
+	                bottom: 15,
+	                color: '#025F8B',
+	                width: width - 60
+	            }
+	        }
+	    ]
+	};
+}
+
+function abreviateName(str){
+	var namesToAbreviate = [
+		'ADMINISTRAÇÃO','PLANEJAMENTO','SUPERVISIONADO','PROFISSIONAL','SOCIOCULTURAL','SEMINÁRIOS','PREVIDÊNCIA','PESQUISA','TÉCNICAS'
+	];
+	var abreviations = [
+		'ADM.','PLAN.','SUPERV.','PROF.','SOCIOC.','SEM.','PREV.','PESQ.','TÉCN.'
+	];
+
+	for(var i=0; i< namesToAbreviate.length; i++){
+		//Ti.API.debug(str);
+		//Ti.API.debug(namesToAbreviate[i]);
+		//Ti.API.debug(str.search( namesToAbreviate[i] ));
+		if( str.search( namesToAbreviate[i] ) > -1){
+			str = str.replace(namesToAbreviate[i],abreviations[i]);
+		}
+	}
+	return ucFirstAllWords( str.replace(/^s+|s+$/g,"") );
+	//return str.replace(/^s+|s+$/g,"");
+}
+
+function ucFirstAllWords( str )
+{
+    var pieces = str.split(" ");
+    for ( var i = 0; i < pieces.length; i++ )
+    {
+    	pieces[i] = pieces[i].toLowerCase();
+        var j = pieces[i].charAt(0).toUpperCase();
+        pieces[i] = j + pieces[i].substr(1);
+    }
+    return pieces.join(" ");
+}
+
+module.exports = NotesList;
---
+++ Resources/ui/login/loginView.js
@@ -0,0 +1,431 @@
+/**
+ * @author jmilanes
+ */
+var width = Ti.Platform.displayCaps.platformWidth;
+var userExists = false;
+function LoginView()
+{
+	// create the login window to hold our login form
+	var loginWindow = Ti.UI.createWindow({
+		//title:'SquidTech login',
+		tabBarHidden:true,
+		navBarHidden:true,
+		exitOnClose: true,
+		backgroundColor: '#fff'
+	});
+
+	var scrollView = Ti.UI.createScrollView({
+		  contentWidth: 'auto',
+		  contentHeight: 'auto',
+		  showVerticalScrollIndicator: true,
+		  showHorizontalScrollIndicator: true,
+		  height: '100%',
+		  width: '100%',
+		  top: 0,
+		  left: 0,
+		  bottom: 50
+	});
+	
+	scrollView.addEventListener('postlayout', function(){
+		scrollView.height = Ti.Platform.displayCaps.platformHeight - 50;
+	});
+
+	scrollView.add( getLoginWindowLabel() );
+	scrollView.add( getLoginForm() );
+	scrollView.add( getLoginDescriptionLabel() );
+	loginWindow.add(scrollView);
+	loginWindow.add( getFooterTemplate() );
+	
+
+	loginWindow.add( activityIndicator );
+	
+	loginWindow.open();
+	
+	return loginWindow;
+}
+
+function getLoginWindowLabel()
+{
+	var labelContainer = Ti.UI.createView({
+	    top: 0,
+	    left: 0,
+	    width: Titanium.UI.SIZE, height: Titanium.UI.SIZE
+	});
+	
+	var logo = Ti.UI.createImageView({
+		image: '/images/logo.png',
+		height: 40,
+		left: 15,
+		top: 20
+	});
+	
+	labelContainer.add(logo);
+	
+	return labelContainer;
+}
+
+function getLoginDescriptionLabel()
+{
+	var labelDescriptionContainer = Ti.UI.createView({
+	    top: 81,
+	    left: 0,
+	    backgroundColor: '#fff',
+	    width: Titanium.UI.SIZE, height: 100
+	});
+	
+	if( userExists ){
+		var image = '/icons/dark_2x/dark_check@2x.png';
+	} else {
+		var image = '/icons/dark_2x/dark_key@2x.png';	
+	}
+	
+	
+	var loginDescriptionIcon = Ti.UI.createImageView({
+		 image: image,
+		 left: 20,
+		 top: 20,
+		 width: 45, height: 45
+	});
+	
+	labelDescriptionContainer.add(loginDescriptionIcon);
+	
+	if( userExists ){
+		var description = 'Clique no botão abaixo para ver suas notas.';
+	} else {
+		var description = 'Informe seu Ra e senha para visualizar sua notas.';	
+	}
+	var loginDescriptionLabel = Ti.UI.createLabel({
+	    color: '#333',
+	    shadow:{
+	        shadowRadius:3,
+	        shadowOpacity:1,
+	        shadowOffset:{x:5, y:5},
+	        shadowColor:"#000000"
+	    },
+	    text: description,
+	    textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+	    top: 20,
+	    left: 80,
+	    width: Titanium.UI.SIZE, height: 60,
+	    font: { fontSize:'14dp' },
+	});
+	
+	labelDescriptionContainer.add(loginDescriptionLabel);
+	
+	return labelDescriptionContainer;
+}
+
+function getLoginForm(){
+	
+	var formContainerView = Ti.UI.createView({
+		top: 181,
+	    left: 0,
+	    width: Titanium.UI.SIZE, height: 450
+	});
+	var username = getRaInput();
+	var password = getPasswordInput();
+	var checkbox = getCheckboxInput();
+	
+
+	var user = getStoredUserCredentioals( username.value );
+	userExists = ( user.rowCount > 0 )? true : false;
+
+	if( userExists ){
+		username.value = user.fieldByName('ra');
+		password.value = user.fieldByName('password');
+	}
+	
+	var loginBtn = getLoginBtn(); 
+	
+	loginBtn.addEventListener('touchend', function(){
+		activityIndicator.show();
+		loginAction({username: username.value, password: password.value, keep: checkbox.value });
+
+	});
+	
+	if( !userExists ){
+		formContainerView.add( username );
+		formContainerView.add( password ); 
+		formContainerView.add( checkbox );
+	} else {
+		loginBtn.top = '50dp';
+		if( debug ){
+			formContainerView.add( getDeleteBtn() );
+		}
+	}
+	
+	formContainerView.add( loginBtn );
+
+	return formContainerView;  
+}
+
+function getLoginWindowActionLabel()
+{
+	var actionLabel = Ti.UI.createLabel({
+	    color: '#fff',
+	    shadowColor: '#aaa',
+	    shadowOffset: {x:5, y:5},
+	    text: 'Entre com suas credenciais da àrea do aluno para ver suas notas.',
+	    textAlign: Ti.UI.TEXT_ALIGNMENT_LEFT,
+	    top: 0,
+	    width: width, height: 80,
+	    font: { fontSize:'20dp',fontWeight:'bold' },
+	});
+	
+	return actionLabel;
+}
+
+function getRaInput()
+{
+	var username = Ti.UI.createTextField({  
+	    color:'#fff',  
+	    top:30,  
+	    left:'5%',  
+	    width: '90%',  
+	    height:80,  
+	    hintText:'RA',
+	    value: '11036102',
+	    backgroundColor: "#025F8B",
+	    keyboardType:	Ti.UI.KEYBOARD_DEFAULT,  
+	    returnKeyType:	Ti.UI.RETURNKEY_DEFAULT,  
+	    borderStyle:	Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+	    borderColor: '#025F8B',
+	    borderWidth: 3,
+	    font: { fontSize: '20dp' }
+	}); 
+
+	username.addEventListener('focus', function(){
+		username.backgroundColor = '#fff';
+		username.color = '#333';
+		username.borderColor = '#025F8B'
+	});
+	
+	username.addEventListener('blur', function(){
+		username.backgroundColor = '#025F8B';
+		username.color = '#fff';
+		username.borderColor = '#025F8B'
+	});
+	
+	return username;	
+}
+
+function getPasswordInput()
+{
+	var password = Ti.UI.createTextField({  
+	    color:'#fff',  
+	    top:130,  
+	    left:'5%',  
+	    width: '90%', 
+	    height:80,  
+	    hintText:'Senha',
+		value:  'senha1',
+	    passwordMask:true,  
+	    backgroundColor: "#025F8B",
+	    keyboardType:	Ti.UI.KEYBOARD_DEFAULT,  
+	    returnKeyType:	Ti.UI.RETURNKEY_DEFAULT,  
+	    borderStyle:	Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+	    borderColor: '#025F8B',
+	    borderWidth: 3,
+	    font: { fontSize: '20dp' }
+	}); 
+	
+	password.addEventListener('focus', function(){
+		password.backgroundColor = '#fff';
+		password.color = '#333';
+		password.borderColor = '#025F8B'
+	});
+	
+	password.addEventListener('blur', function(){
+		password.backgroundColor = '#025F8B';
+		password.color = '#fff';
+		password.borderColor = '#025F8B'
+	});
+	
+	return password;
+	
+}
+
+function getCheckboxInput(){
+	
+	/* var checkboxContainer = Ti.UI.createView({
+	    
+	    width: width, height: 80
+	}); */
+	
+	var checkbox = Ti.UI.createSwitch({
+		style : Ti.UI.Android.SWITCH_STYLE_CHECKBOX,
+	    title : 'Salvar credenciais ?',
+	    value : false,
+	    top : 230,
+	    height : 50,
+	    left:'5%',  
+	    width: '90%', 
+	    color: '#333'
+	});
+	
+	//checkboxContainer.add(checkbox);
+	
+	return checkbox;
+}
+
+function getLoginBtn()
+{
+	if( userExists ){
+		var text = 'Ver notas';
+	} else {
+		var text = 'Enviar';
+	}
+	
+	var loginBtn = Ti.UI.createButton({  
+		    title: text,  
+		    top:300, 
+		    left:'5%',  
+	    	width: '90%',   
+		    height:80,  
+		    borderRadius:1, 
+		    backgroundColor: '#2292CE',
+		    color: '#fff',
+		    font: {fontFamily:'Tahoma',fontWeight:'bold',fontSize:'20dp'},
+		    borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+		    borderColor: '#2292CE',
+		    borderWidth: 3
+	}); 
+	
+	loginBtn.addEventListener('touchstart', function(){
+		loginBtn.backgroundColor = '#025F8B';
+		loginBtn.color = '#fff';
+		loginBtn.borderColor = '#2292CE';
+	});
+
+	return loginBtn;
+	
+}
+
+function getDeleteBtn()
+{
+	var deleteBtn = Ti.UI.createButton({  
+		    title:'remover credenciais',  
+		    top:250, 
+		    left:'5%',  
+	    	width: '90%',   
+		    height:80,  
+		    borderRadius:1, 
+		    backgroundColor: '#8F1111',
+		    color: '#fff',
+		    font: {fontFamily:'Tahoma',fontWeight:'bold',fontSize:'20dp'},
+		    borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+		    borderColor: '#8F1111',
+		    borderWidth: 3
+	}); 
+	
+	deleteBtn.addEventListener('click', function(){
+		deleteUserCredentials();
+	});
+
+	return deleteBtn;	
+}
+
+function getExistingCredentialsView(){
+
+}
+
+function loginAction(loginInfo){
+	var data = getDataFromDb(loginInfo.username);
+	Ti.API.debug(data);
+	if( false !== data ){
+		return getNotesWindow(data);
+	} 
+	else {
+		var url = 'http://squidtech.layoutz.com.br/cesunotas_wbs/webservice.php';
+	 	var client = Ti.Network.createHTTPClient({
+		     // function called when the response data is available
+		     onload : function(e) {
+		         Ti.API.debug(loginInfo.keep);
+		         if( loginInfo.keep ){
+		         	updateUserCredentilas(loginInfo);
+		         }	         
+		         updateDataInDb({
+		         			user: loginInfo.username,
+		         			responseText: this.responseText
+		         		});
+		         
+		         return getNotesWindow(this.responseText);
+		     },
+		     // function called when an error occurs, including a timeout
+		     onerror : function(e) {
+		         //Ti.API.debug(e.error);
+		         return false;
+		     },
+		     timeout : 5000  // in milliseconds
+		 });
+		 // Prepare the connection.
+		 client.open("POST", url);
+		 // Send the request.
+		 client.send(loginInfo);
+	}
+}
+
+function getNotesWindow(notes)
+{
+	//Ti.API.info(notes);
+	var NotesList = require('/ui/common/NotesList');
+	/*var currentWin = Ti.UI.currentWindow;
+	if( currentWin !== null ){
+		currentWin.close();
+	}*/
+	return new NotesList( notes );
+	//return true;	
+}
+
+function updateUserCredentilas(loginInfo){
+	var db = Ti.Database.open('squidtech.sqlite');
+	Ti.API.debug(db);
+	var results = db.execute('REPLACE INTO credentials ( ra, password ) VALUES("'+loginInfo.username+'","'+loginInfo.password+'")');
+	db.close();
+	return results;
+}
+
+function getStoredUserCredentioals(ra){
+	var db = Ti.Database.open('squidtech.sqlite');
+	var results = db.execute('SELECT * FROM credentials WHERE ra = "'+ra+'"');
+	db.close();
+	if( results.isValidRow() ){
+		return results;
+	}
+	return false;	
+}
+
+function getDataFromDb(ra){
+	var db = Ti.Database.open('squidtech.sqlite');
+	var results = db.execute('SELECT * FROM data WHERE id = "'+ra+'"');
+	db.close();
+	if( results.isValidRow() ){
+		return results.fieldByName('json');
+	}
+	return false;
+}
+
+function updateDataInDb(data){
+	var db = Ti.Database.open('squidtech.sqlite');	
+	var lasttime = new Date();
+	value = data.responseText;
+	var results = db.execute("REPLACE INTO data ( id, json, lasttime ) VALUES('"+data.user+"','"+value.toString()+"', '"+lasttime.toString("yyyy/mm/dd HH:MM:ss")+"')");
+	db.close();
+	return results;
+}
+
+function deleteDataInDb(){
+	var db = Ti.Database.open('squidtech.sqlite');
+	var results = db.execute('DELETE FROM data WHERE 1 = 1');
+	db.close();
+	return true;
+}
+
+function deleteUserCredentials(){
+	var db = Ti.Database.open('squidtech.sqlite');
+	var results = db.execute('DELETE FROM credentials WHERE 1 = 1');
+	db.close();
+	return true;
+}
+
+module.exports = LoginView;
